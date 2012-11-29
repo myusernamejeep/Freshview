27c10919b528c5d382a6754107075b82e4773cf8
Ethan Fang <fcq_731@hotmail.com>
2012-11-30 01:20:30 +1100

    Fixed Topics publish and rendering

--- client/client.js
+++ client/client.js
@@ -1,3 +1,45 @@
 Template.freshview.topics = function() {
   return Topics.find({});
 };
+
+Template.freshview.events({
+  'click .new-topic': function (e) {
+    e.preventDefault();
+    Router.navigate('new');
+  }
+});
+
+formData = function(form) {
+  var data = {};
+  var array = $(form).serializeArray();
+  _.each(array, function(objField){
+    data[objField.name] = objField.value;
+  });
+  return data;
+}
+
+Template.new.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    data = formData(e.currentTarget);
+    console.log(data);
+    topic_id = Topics.insert(data);
+    console.log(topic_id);
+  }
+});
+
+FVRouter = Backbone.Router.extend({
+  routes: {
+    "new": "createNew"
+  },
+  createNew: function() {
+    $('body').html(Template.new());
+  }
+});
+
+
+var Router = new FVRouter;
+
+Meteor.startup(function() {
+  Backbone.history.start({pushState:true});
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 Meteor.startup(function() {
-  if (Topics.find().count() === 0) {
-    var titles = ["Ada Lovelace", "Grace Hopper", "Marie Curie", "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
-    for (var i = 0; i < titles.length; i++)
-      Topics.insert({
-        title : titles[i],
-        detail : titles[i],
-      });
-  }
+  // if (Topics.find().count() === 0) {
+    // var titles = ["Ada Lovelace", "Grace Hopper", "Marie Curie", "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+    // for (var i = 0; i < titles.length; i++)
+      // Topics.insert({
+        // title : titles[i],
+        // content : titles[i],
+      // });
+  // }
+  // Topics.remove({});
 }); 
